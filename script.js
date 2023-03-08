41e5312016fd294ad5de58fc12f649e41cab591a
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-03-08 19:41:59 +0100

    Completed basic form UI and create basic JS

---
+++ script.js
@@ -0,0 +1,9 @@
+let nameValidation = document.querySelector('#firstName');
+
+//function check () {
+    if (nameValidation.value.length !== 0) {
+        nameValidation.classList.add('valid');
+    };
+//}
+
+//check();
